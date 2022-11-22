2557a0dcbc011fe17e75370fc052734133f6bed0
Rafael Reis <rafaeloliveira63@yahoo.com>
2022-11-21 23:12:09 -0300

    adicionando funcao de informacao de telefone

--- src/shared/mixins/HandlePhone.test.js
+++ src/shared/mixins/HandlePhone.test.js
@@ -1,27 +1,19 @@
-import { HandlePhone} from './HandlePhone'
+import { HandlePhone } from "./HandlePhone";
 
 describe("Teste Da função de Telefone do Orgão de Apoio", () => {
-
-    test('Infancia',()=>{  
-      expect(HandlePhone('infancia')).toBe('(32) 3690-7397')
-  
-    })
-    test('Adolescencia',()=>{  
-      expect(HandlePhone('adolescencia')).toBe('(32) 3690-7171')
-  
-    })
-    test('Adulto',()=>{  
-      expect(HandlePhone('adulto')).toBe('(32) 3690-8256')
-  
-    })
-    test('Velhice',()=>{  
-      expect(HandlePhone('velhice')).toBe('(32) 3250-2827')
-  
-    })
-    test('Valor Inválido',()=>{  
-      expect(HandlePhone('xptoxpto')).toBe('Valor Inválido')
-  
-    })
-  
-  
-  })
+  test("Infancia", () => {
+    expect(HandlePhone("infancia")).toBe("(32) 3690-7397");
+  });
+  test("Adolescencia", () => {
+    expect(HandlePhone("adolescencia")).toBe("(32) 3690-7171");
+  });
+  test("Adulto", () => {
+    expect(HandlePhone("adulto")).toBe("(32) 3690-8256");
+  });
+  test("Velhice", () => {
+    expect(HandlePhone("velhice")).toBe("(32) 3250-2827");
+  });
+  test("Valor Inválido", () => {
+    expect(HandlePhone("xptoxpto")).toBe("Valor Inválido");
+  });
+});
